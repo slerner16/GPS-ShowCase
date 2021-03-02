b53804e2708a2782caa57bb39dbde2672b504094
Ben Stavig <bstavig5299@floridapoly.edu>
2021-03-02 13:01:54 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace GPS_ShowCase
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StartButt = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // StartButt
+            // 
+            this.StartButt.Location = new System.Drawing.Point(600, 63);
+            this.StartButt.Name = "StartButt";
+            this.StartButt.Size = new System.Drawing.Size(75, 23);
+            this.StartButt.TabIndex = 0;
+            this.StartButt.Text = "Start";
+            this.StartButt.UseVisualStyleBackColor = true;
+            this.StartButt.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(551, 546);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(953, 582);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.StartButt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartButt;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GPS_ShowCase
+{
+    public partial class Form1 : Form
+    {
+        int i=0;
+        private readonly object x = new object();
+        public Form1()
+        {
+            InitializeComponent();
+            listView1.Columns.Add("MyColumn", -2, HorizontalAlignment.Left);
+            listView1.FullRowSelect = true;
+            listView1.GridLines = true;
+            listView1.View = System.Windows.Forms.View.List;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            SerialPort mySerialPort = new SerialPort();
+
+                mySerialPort.BaudRate = 4800;
+                mySerialPort.Parity = Parity.None;
+                mySerialPort.StopBits = StopBits.One;
+                mySerialPort.DataBits = 8;
+                mySerialPort.Handshake = Handshake.None;
+                mySerialPort.RtsEnable = true;
+
+                mySerialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+
+
+                foreach (string s in SerialPort.GetPortNames())
+                {
+                    Console.WriteLine(s);
+                    mySerialPort.PortName = s;
+                }
+
+                mySerialPort.Open();
+                   
+        }
+        private void DataReceivedHandler(
+                                object sender,
+                                SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            string indata = sp.ReadLine();
+            string[] SplitData = indata.Split(',');
+            if (SplitData[0] == "$GPRMC")
+            {
+                Console.WriteLine(indata);
+                Console.WriteLine("Latitude: " , SplitData[3]);
+                Console.WriteLine("Longitude: " , SplitData[5]);
+                string lat = SplitData[3];
+                string longi = SplitData[5];
+                Thread t = new Thread(()=>AddToList(lat, longi));
+                t.Start();
+                t.Join();
+
+            }
+        }
+        private void AddToList(string lat, string longi)
+        {
+            lock (x)
+            {
+                string mylab;
+                mylab = "Latitude: " + lat + "\nLongitude: " + longi;
+                ListViewItem MyItem = new ListViewItem();
+                MyItem.Text = mylab;
+                listView1.Invoke(new MethodInvoker(delegate { 
+                listView1.Items.Add(MyItem);
+                listView1.AutoResizeColumn(0, ColumnHeaderAutoResizeStyle.ColumnContent); ;
+                }));
+                
+                
+                i++;
+            }
+            
+        }
+    }
+}
